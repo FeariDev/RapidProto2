4c8378723e9e7d87bbb26469e6ffba34dcaad4e0
FeariDev <140981551+FeariDev@users.noreply.github.com>
2025-09-05 12:34:24 +0300

    folder structure

---
+++ Assets/_stuff/scripts/BaseFishingRodSO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Base_Fishing_Rod_Settings", menuName = "FishingRods/Base Fishing Rod")]
+public class BaseFishingRodSO : ScriptableObject
+{
+    
+}
