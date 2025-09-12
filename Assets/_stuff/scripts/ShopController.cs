c7bec44ed05e0815c8fd0b9b3eef003641051566
otpx5a <otpx5a@gmail.com>
2025-09-12 12:34:27 +0300

    Shop Code
    + Working shop code

--- Assets/_stuff/scripts/ShopController.cs
+++ Assets/_stuff/scripts/ShopController.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ShopController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+ 
+
+
+    [System.Serializable]
+    public class ShopItem
     {
-        
+        public string itemName;
+        public float price;
+        public Button buyButton;
     }
 
-    // Update is called once per frame
-    void Update()
+    public ShopItem[] shopItems; // Assign 6 items in Inspector
+
+    private void Start()
     {
-        
+        foreach (ShopItem item in shopItems)
+        {
+            ShopItem capturedItem = item; // avoid closure issue
+            item.buyButton.onClick.AddListener(() => BuyItem(capturedItem));
+        }
+    }
+
+    void BuyItem(ShopItem item)
+    {
+        if (Player.Instance.money >= item.price)
+        {
+            Player.Instance.ChangeMoney(-item.price);
+            Debug.Log("Bought: " + item.itemName);
+
+            // Disable after buying
+            item.buyButton.interactable = false;
+        }
+        else
+        {
+            Debug.Log("Not enough money to buy: " + item.itemName);
+        }
     }
 }
+
+
