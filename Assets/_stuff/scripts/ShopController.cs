fcc2830c1ac86a0017514eb9059bf08471102676
FeariDev <140981551+FeariDev@users.noreply.github.com>
2025-09-25 11:50:23 +0300

    Final

--- Assets/_stuff/scripts/ShopController.cs
+++ Assets/_stuff/scripts/ShopController.cs
@@ -1,8 +1,9 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 public class ShopController : MonoBehaviour
 {
- 
+    public Sprite outOfOrderSprite;
 
 
     [System.Serializable]
@@ -11,16 +12,30 @@ public class ShopController : MonoBehaviour
         public string itemName;
         public float price;
         public Button buyButton;
+        public bool isSoldOut;
     }
 
     public ShopItem[] shopItems; // Assign 6 items in Inspector
+    public static bool[] soldOutItems;
 
     private void Start()
     {
+        ResizeSoldOutList();
+
+        int index = 0;
         foreach (ShopItem item in shopItems)
         {
             ShopItem capturedItem = item; // avoid closure issue
+            item.isSoldOut = soldOutItems[index];
             item.buyButton.onClick.AddListener(() => BuyItem(capturedItem));
+
+            if (item.isSoldOut)
+            {
+                item.buyButton.GetComponent<Image>().sprite = outOfOrderSprite;
+                item.buyButton.interactable = false;
+            }
+
+            index++;
         }
     }
 
@@ -34,21 +49,34 @@ public class ShopController : MonoBehaviour
             if (item.itemName == "Small Energy Drink")
             {
                 inventory.AddItem(PlayerInventory.Items.EnergyDrink1, 1);
+                soldOutItems[0] = true;
             }
             else if (item.itemName == "Strong Rod")
             {
                 inventory.AddItem(PlayerInventory.Items.FishingRod1, 1);
+                soldOutItems[4] = true;
             }
             else if (item.itemName == "Automatic Rod")
             {
                 inventory.AddItem(PlayerInventory.Items.FishingRod2, 1);
+                soldOutItems[3] = true;
             }
-            // Disable after buying
+            item.buyButton.GetComponent<Image>().sprite = outOfOrderSprite;
             item.buyButton.interactable = false;
         }
         else
         {
             Debug.Log("Not enough money to buy: " + item.itemName);
+        }
+    }
+
+    [Button]
+    void ResizeSoldOutList()
+    {
+        Array.Resize(ref soldOutItems, shopItems.Length);
+        foreach (bool b in soldOutItems)
+        {
+            Debug.Log(b);
         }
     }
 }
